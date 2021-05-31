5aef3c338d0566556522fc6fc418f3de8bf96776
Mickaël Allonneau <mickael.allonneau@gmail.com>
2021-06-01 00:07:12 +0200

    imp: run on all twitch subdomains
    Makes the extension run on 'clips.twitch.tv' too, and also any other
    subdomain that I'm currently unaware of or that don't exist yet.

--- background-script.js
+++ background-script.js
@@ -1,7 +1,7 @@
 chrome.runtime.onMessage.addListener((message, sender, reply) => {
   if (message.type === 'mute_other_streams') {
     // Mute all Twitch tabs except the one that sent the message
-    chrome.tabs.query({ url: 'https://www.twitch.tv/*' }, twitchTabs => {
+    chrome.tabs.query({ url: 'https://*.twitch.tv/*' }, twitchTabs => {
       if (chrome.runtime.lastError) {
         console.error(chrome.runtime.lastError.message);
         return;
