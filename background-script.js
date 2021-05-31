2f96a064172b25f139fc585c65c950b224af3035
Mickaël Allonneau <mickael.allonneau@gmail.com>
2021-06-01 00:02:09 +0200

    misc(bs): handle tabs querying errors

--- background-script.js
+++ background-script.js
@@ -2,6 +2,11 @@ chrome.runtime.onMessage.addListener((message, sender, reply) => {
   if (message.type === 'mute_other_streams') {
     // Mute all Twitch tabs except the one that sent the message
     chrome.tabs.query({ url: 'https://www.twitch.tv/*' }, twitchTabs => {
+      if (chrome.runtime.lastError) {
+        console.error(chrome.runtime.lastError.message);
+        return;
+      }
+
       twitchTabs.forEach(tab => chrome.tabs.update(tab.id, {
         muted: tab.id !== sender.tab.id
       }))
